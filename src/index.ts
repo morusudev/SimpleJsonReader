b56ff918723b7e138437ab0560b7b14169dacba2
Vinicius Lima <gvlima.contato@gmail.com>
2025-06-17 18:06:45 -0300

    :rocket: Up Archives TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,3 @@
-//=============== Hello World ===============
-
-// const name1 = (param: string)=> `Olá ${param} como você vai?`
-
-// console.log(name1("Hello World"))
-
-
-//=============== Calculadora ===============
-
-// const numberOne: number = 10
-
-// const numberTwo: number = 7
-
-// const sum = (numberOne: number, numberTwo: number) => numberOne + numberTwo
-// const subtrain = (numberOne: number, numberTwo: number) => numberOne - numberTwo
-// const divide = (numberOne: number, numberTwo: number) => numberOne / numberTwo
-// const multiply = (numberOne: number, numberTwo: number) => numberOne * numberTwo
-
-// console.log(sum(numberOne, numberTwo))
-// console.log(subtrain(numberOne, numberTwo))
-// console.log(divide(numberOne, numberTwo))   
-// console.log(multiply(numberOne, numberTwo))
-
-// ================ Desafio ============
-
 import express, {Request, Response} from "express"
 import fs from "fs"
 import { request } from "http"
